70c89cbe44c7fbfaf307d79e109af89aadd798ef
Sulaiman Alhabib <suldev8@gmail.com>
2019-07-21 12:10:36 +0300

    Migrate Loan model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_21_090128) do
+ActiveRecord::Schema.define(version: 2019_07_21_090924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2019_07_21_090128) do
     t.string "name"
     t.boolean "loaned"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "loans", force: :cascade do |t|
+    t.string "status"
+    t.integer "borrower_id"
+    t.integer "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
