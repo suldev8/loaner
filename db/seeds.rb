02761e374c9af70e934e815e6d49724d977b756c
Sulaiman Alhabib <suldev8@gmail.com>
2019-07-21 16:00:55 +0300

    Add data to the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user1 = User.create(email: "sah232@hotmail.com", password: "432dse2d", name: "Sulaiman", phone: "34325223", city: "Riyadh" )
+user2 = User.create(email: "jfdj33@hotmail.com", password: "dfwed3dd", name: "Badur", phone: "3453522", city: "Riyadh" )
+user3 = User.create(email: "fdge55@hotmail.com", password: "fdfdf3fg", name: "Nasser", phone: "3452345", city: "Riyadh" )
+
+item1_user1 = Item.create(name: "TV", loaned: false, user_id: user1.id )
+item2_user1 = Item.create(name: "Microwave", loaned: false, user_id: user1.id )
+
+loan1_user2 = Loan.create(status: "pending", user_id: user2.id, item_id: item1_user1.id )
+loan2_user2 = Loan.create(status: "pending", user_id: user2.id, item_id: item2_user1.id )
