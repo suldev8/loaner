23540c7eb83b49fa8c89fc66637142a257c13723
Sulaiman Alhabib <suldev8@gmail.com>
2019-07-21 22:46:52 +0300

    Make nested routes for users items and loans

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'loans/index'
-  get 'loans/show'
-  get 'loans/edit'
-  get 'loans/new'
-  get 'items/index'
-  get 'items/show'
-  get 'items/edit'
-  get 'items/new'
+  root 'home#index'
+  # get 'loans/index'
+  # get 'loans/show'
+  # get 'loans/edit'
+  # get 'loans/new'
+  # get 'items/index'
+  # get 'items/show'
+  # get 'items/edit'
+  # get 'items/new'
   devise_for :users
+
+  resources :users do 
+    resources :items
+    resources :loans
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
