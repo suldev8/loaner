2f115af16cfed2e170c0ab1492527289912be0c3
Sulaiman Alhabib <suldev8@gmail.com>
2019-07-23 18:39:48 +0300

    Add in requests and out requests

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,16 @@ Rails.application.routes.draw do
   # get 'items/show'
   # get 'items/edit'
   # get 'items/new'
+
+  get 'loans/out_requests', to: 'loans#out_requests', as: 'out_requests'
+  get 'loans/in_requests', to: 'loans#in_requests', as: 'in_requests'
   devise_for :users
 
   resources :users do 
     resources :items
     resources :loans
   end
+
+  get "/change", to: "loans#changest"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
