d9878def57326687ea5779454c94cfa7b243d3f7
Sulaiman Alhabib <suldev8@gmail.com>
2019-07-23 18:38:53 +0300

    Add loans actions

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,13 +1,51 @@
 class LoansController < ApplicationController
+  before_action :set_loan, only: [ :destroy]
+  before_action :authenticate_user!, only: [ :changest, :create, :destroy ]
   def index
+    @loans =Loan.where(user_id: params[:user_id])
   end
 
-  def show
+  def out_requests
+    @loans = Loan.where(user_id: current_user.id)
   end
 
-  def edit
+  def in_requests
+    @items = current_user.items
   end
 
-  def new
+  def changest
+    @loan = Loan.find(params[:id])
+    @loan.update_attribute(:status, params[:status])
+    @loan.save
+    redirect_to request.referrer
+  end 
+
+  def create
+    @loan = current_user.loans.new(loan_params)
+
+    if @loan.save
+      redirect_to out_requests_path
+    else
+      puts "can't save"
+    end
   end
+
+  def destroy
+     if @loan.destroy
+      redirect_to out_requests_path
+     end
+  end
+
+
+  private
+    def set_loan
+      @loan = Loan.find(params[:id])
+    end
+
+    def update_loan_params
+      params.require(:loan).permit(:item_id, :status)
+    end
+    def loan_params
+      params.require(:loan).permit(:item_id)
+    end
 end
